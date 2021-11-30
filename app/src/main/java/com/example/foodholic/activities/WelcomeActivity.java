36ea06cffe0f9cbc1054dca12ad472659bbb1b5b
Badhon <aasbadhon268@gmail.com>
2021-11-30 13:53:17 +0600

    FoodHolic

---
+++ app/src/main/java/com/example/foodholic/activities/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.foodholic.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodholic.R;
+
+import static com.example.foodholic.R.*;
+import static com.example.foodholic.R.layout.*;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.activity_welcome);
+    }
+}
